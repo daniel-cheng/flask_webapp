d02e631c745d2a1f351850f12acb4cc39dce6509
Daniel Cheng <dcheng334@gmail.com>
2014-05-22 11:55:40 -0700

    Working!

--- sample_webpage.py
+++ sample_webpage.py
@@ -1,12 +1,21 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
  
 app = Flask(__name__)
- 
+
 @app.route('/')
-def hello():
-	return 'Hello World!'
+def hello_world():
+	# print("HTML Accessed")
+	return render_template('home.html')
  
+
+# @app.route('/stylesheet')
+# def stylesheet():
+    # with open("static\stylesheets\stylesheet.css", "r") as css:
+        # css_string = css.read()
+	# print("CSS Accessed")
+    # return css_string    
+	
 if __name__ == '__main__':
 	# Bind to PORT if defined, otherwise default to 5000.
 	port = int(os.environ.get('PORT', 5000))
