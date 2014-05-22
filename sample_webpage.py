724e492f468c84bede5299cadee2a7dd43fed8ad
Daniel Cheng <dcheng334@gmail.com>
2014-05-22 09:04:40 -0700

    new Procfile

--- sample_webpage.py
+++ sample_webpage.py
@@ -1,8 +1,19 @@
-import os
-from flask import Flask
-
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hello World!'
+def hello_world():
+		print("HTML Accessed")
+	return render_template('home.html')
+ 
+
+@app.route('/stylesheet')
+def stylesheet():
+    with open("static\stylesheets\stylesheet.css", "r") as css:
+        css_string = css.read()
+	print("CSS Accessed")
+    return css_string    
+
+# if __name__ == '__main__':
+	# print("Application started")
+    # app.run()
