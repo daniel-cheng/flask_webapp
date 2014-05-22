414d94805b5fac79cd87d16c71d8cc427effa687
Daniel Cheng <dcheng334@gmail.com>
2014-05-22 09:00:51 -0700

    Testing debug

--- sample_webpage.py
+++ sample_webpage.py
@@ -1,20 +1,8 @@
-from flask import Flask, render_template
+import os
+from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-	return render_template('home.html')
-	
-def hello_world1():
-    with open("templates\home.html", "r") as html:
-        html_string = html.read()
-    return html_string      
-
-@app.route('/stylesheet')
-def stylesheet():
-    with open("static\stylesheets\stylesheet.css", "r") as css:
-        css_string = css.read()
-    return css_string    
-
-if __name__ == '__main__':
-    app.run()
+def hello():
+    return 'Hello World!'
