3009fa1617b60c524a15898a3469c7206c98908c
Daniel Cheng <dcheng334@gmail.com>
2014-05-24 01:07:48 -0700

    changed webpage, css

--- sample_webpage.py
+++ sample_webpage.py
@@ -1,4 +1,4 @@
-import os
+import os, json, urllib2
 from flask import Flask, render_template
  
  # Create Flask instance using current shell name (this allows running on both your computer and on a server)
@@ -10,7 +10,11 @@ app = Flask(__name__)
 def hello_world():
 	# print("HTML Accessed")
 	#Flask utilizes Jinja2 to dynamically parse html files. render_template bascially returns a string of the html file, but with subsituted information from Jinja2.
-	return render_template('home.html')
+	url = "https://www.googleapis.com/youtube/v3/videos?id=7lCDEYXw3mM&key=AIzaSyCvmUuH7_MNaGQG5Sxbb6jhyVTa_5aLCA8&part=snippet,contentDetails,statistics,status"
+	data = json.load(urllib2.urlopen(url))
+	viewCount = data['items'][0]['statistics']['viewCount']
+	print viewCount
+	return render_template('home.html', viewCount=viewCount, dummy="dummy")
  
 # Example of adding pages. Uncomment for fun.
 # @app.route('/stylesheet')
@@ -23,5 +27,5 @@ def hello_world():
 if __name__ == '__main__':
 	# Bind to PORT if defined, otherwise default to 5000.
 	port = int(os.environ.get('PORT', 5000))
-	app.run(host='0.0.0.0', port=port)
+	app.run(host='0.0.0.0', port=port, debug=True)
  
