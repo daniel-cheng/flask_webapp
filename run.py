4ab933b369c95372c62cc53769e37e8e57e9b781
Daniel Cheng <dcheng334@gmail.com>
2014-05-24 21:58:14 -0700

    Pretty much working

--- run.py
+++ run.py
@@ -1,9 +1,12 @@
+from urllib2 import URLError
 import os, json, urllib2
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
 from forms import ChannelForm
+import string
  
  # Create Flask instance using current shell name (this allows running on both your computer and on a server)
 app = Flask(__name__)
+app.secret_key = "secret_key"
 YOUR_API_KEY = "AIzaSyCvmUuH7_MNaGQG5Sxbb6jhyVTa_5aLCA8"
 # .route Decorator function creates the actual URL structure of the site - see example below for adding additional pages
 
@@ -11,25 +14,35 @@ YOUR_API_KEY = "AIzaSyCvmUuH7_MNaGQG5Sxbb6jhyVTa_5aLCA8"
 def CostlyBanana():
     form = ChannelForm(request.form)
     data = ""
-    data2= ""
+    channelsDetails = []
+    channelname=""
     if request.method == 'POST':
         #get channel, channel url
         channelname = form.channelname.data
-        url = "https://www.googleapis.com/youtube/v3/search?part=snippet&q={channelname}&type=channel&key={api_key}".format(channelname=channelname,api_key=YOUR_API_KEY)
+        url = "https://www.googleapis.com/youtube/v3/search?part=snippet&q={channelname}&type=channel&key={api_key}".format(channelname=channelname, api_key=YOUR_API_KEY)
         data = json.load(urllib2.urlopen(url))
-
-        #gets channel id, and channel title/video url
-        channelId = data['items'][0]['snippet']['channelId']
-        channel = "https://www.googleapis.com/youtube/v3/activities?part=snippet&channelId={channelId}&key=AIzaSyCvmUuH7_MNaGQG5Sxbb6jhyVTa_5aLCA8".format(channelId=channelId)
-        data2 = json.load(urllib2.urlopen(channel))
-
-        #
-
-
-
-    #channelName = data['items'][0]['statistics']['viewCount']
-    #thumbnail = data['items'][0]['snippet']['thumbnails']['medium']['url']
-    return render_template('home.html', data=data, form=form,data2=data2)
+        try:
+            channel = []
+            channel.append(data['items'][0]['snippet']['thumbnails']['default']['url'])
+            channel.append(data['items'][0]['snippet']['title'])
+            channel.append(data['items'][0]['snippet']['channelId'])
+            search_url = "https://www.googleapis.com/youtube/v3/subscriptions?key=AIzaSyCvmUuH7_MNaGQG5Sxbb6jhyVTa_5aLCA8&part=snippet&channelId={channelid}&key={api_key}".format(channelid=data['items'][0]['snippet']['channelId'],api_key=YOUR_API_KEY)
+            data = json.load(urllib2.urlopen(url))
+            channelsDetails.append(channel)
+            count = 0
+            while count < data['pageInfo']['totalResults']:
+                results = 0
+                while results < data['pageInfo']['resultsPerPage'] and results + count < data['pageInfo']['totalResults']:
+                    channelsDetails.append([data['items'][results]['snippet']['thumbnails']['default']['url'], data['items'][results]['snippet']['title'], data['items'][results]['snippet']['channelId']])
+                    count += 1
+                    results += 1
+                try:
+                    search_url = "https://www.googleapis.com/youtube/v3/subscriptions?key=AIzaSyCvmUuH7_MNaGQG5Sxbb6jhyVTa_5aLCA8&part=snippet&channelId={channelid}&nextPageToken={next_page_token}&key={api_key}".format(channelid=data['items'][results]['snippet']['channelId'][count], next_page_token=data['pageInfo']['nextPageToken'], api_key=YOUR_API_KEY)
+                except:
+                    break
+        except:
+            raise
+    return render_template('home.html', data=channelsDetails, form=form)
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
